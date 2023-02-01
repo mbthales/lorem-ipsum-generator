fda9f948f940e81c2469d920e492e555e0df3708
Thales Maia <mbthales10@gmail.com>
2023-02-01 19:08:49 -0300

    Adicionei uma funcionalidade de copiar o texto e terminei a parte de lógica

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { LoremIpsum } from "lorem-ipsum";
-
-const generateLoremIpsumBtn = document.getElementById("btn-generate-lorem-ipsum");
+import ClipboardJS  from "clipboard";
 
 const getLoremIpsumTypeInputValue = () => {
   const loremIpsumTypesInputs: NodeListOf<HTMLInputElement> = document.querySelectorAll("input[name=lorem-ipsum-type]");
@@ -22,13 +21,40 @@ const getLoremIpsumQuantityInputValue = () => {
   return parseFloat(loremIpsumQuantityInputValue);
 };
 
-generateLoremIpsumBtn?.addEventListener("click", () => {
-  const loremIpsumType = getLoremIpsumTypeInputValue();
-  const loremIpsumQuantity = getLoremIpsumQuantityInputValue();
-})
+const getLoremIpsum = (type: string, quantity: number) => {
+  const lorem = new LoremIpsum();
+  lorem.generateWords(1);
 
+  if(type === "word"){
+    return lorem.generateWords(quantity);
+  } else if(type === "sentence"){
+    return lorem.generateSentences(quantity);
+  } else if(type === "paragraph"){
+    return lorem.generateParagraphs(quantity);
+  } return lorem.generateWords(quantity);
+};
 
-const lorem = new LoremIpsum({
+const putLoremIpsumInTextArea = (loremIpsum: string) => {
+  const loremIpsumTextArea = <HTMLInputElement>document.getElementById("lorem-ipsum-text");
 
+  loremIpsumTextArea.value = loremIpsum;
+};
 
-})
+const copyLoremIpsum = () =>{
+  new ClipboardJS('#btn-copy-lorem-ipsum');
+};
+
+const generateLoremIpsumEvent = () => {
+  const generateLoremIpsumBtn = document.getElementById("btn-generate-lorem-ipsum");
+
+  generateLoremIpsumBtn?.addEventListener("click", () => {
+    const loremIpsumType = getLoremIpsumTypeInputValue();
+    const loremIpsumQuantity = getLoremIpsumQuantityInputValue();
+    const loremIpsum = getLoremIpsum(loremIpsumType, loremIpsumQuantity);
+  
+    putLoremIpsumInTextArea(loremIpsum);
+  });
+}
+
+generateLoremIpsumEvent();
+copyLoremIpsum();
